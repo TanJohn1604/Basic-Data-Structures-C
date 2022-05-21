c0f263cb677cc49b60498797d7f1e571750eecc2
TanJohn <lehoangtan1604@gmail.com>
2022-05-21 13:13:15 +0700

    LinkedListInsert

---
+++ LinkedListInsert.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void insert(int n);
+void print();
+
+struct Node{
+    int data;
+    struct Node *link;
+};
+struct Node* p_head;
+int main(){
+    p_head=NULL;
+    int n=1;
+    while(n){
+    printf("Nhap so can chen \n");
+    scanf("%d",&n);
+    insert(n);
+    print();
+    }
+    return 1;
+}
+
+void insert(int n){
+    if(p_head==NULL){
+        struct Node *temp=(struct Node*)malloc(sizeof(struct Node));
+        temp->data=n;
+        temp->link=NULL;
+        p_head=temp;
+    }
+    else{
+        struct Node *temp= (struct Node*)malloc(sizeof(struct Node));
+        temp->data=n;
+        temp->link=NULL;
+
+        struct Node *temp1=p_head;
+        while(temp1->link!=NULL){
+            temp1=temp1->link;
+        }
+        temp1->link=temp;
+
+
+    }
+}
+void print(){
+    struct Node *temp1=p_head;
+    printf("day so linkedlist la ");
+    while(temp1!=NULL){
+        printf("  %d",temp1->data);
+        temp1=temp1->link;
+    }
+    printf("\n");
+}
